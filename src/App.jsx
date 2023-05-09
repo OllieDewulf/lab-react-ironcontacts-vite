150900a3e0eda107870f4d09fec4201ab7cc1514
Olivier Dewulf <olivierdewulf@Oliviers-MacBook-Pro.local>
2023-05-09 18:20:08 +0200

    iteration 3 halfway thru

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,72 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import contacts from './contacts.json'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [Contacts, setContacts] = useState(contacts.slice(0, 5));
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+  const handleClick = () => {
+    const restOfContacts = contacts.filter(element => !contacts.includes(element))
+
+    const randomContact = Math.random() * restOfContacts.length
+  }
+
+  const imgWidth = {
+    height: '200px',
+  }
+  
+  return <div className='App'>
+    <h1>Iron Contacts</h1>
+    <button onClick={handleClick}>Add Random Contact</button>
+    <table>
+      <thead>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won an Oscar</th>
+          <th> Won an Emmy</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td><img style={imgWidth} src={contacts[0].pictureUrl} alt=''></img></td>
+          <td>{contacts[0].name}</td>
+          <td>{contacts[0].popularity}</td>
+          <td>{contacts[0].wonOscar ? '🏆' : 'nope'}</td>
+          <td>{contacts[0].wonEmmy ? '🏆' : 'nope'}</td>
+        </tr>
+        <tr>
+        <td><img style={imgWidth} src={contacts[1].pictureUrl} alt=''></img></td>
+          <td>{contacts[1].name}</td>
+          <td>{contacts[1].popularity}</td>
+          <td>{contacts[1].wonOscar ? '🏆' : 'nope'}</td>
+          <td>{contacts[1].wonEmmy ? '🏆' : 'nope'}</td>
+        </tr>
+        <tr>
+        <td><img style={imgWidth} src={contacts[2].pictureUrl} alt=''></img></td>
+          <td>{contacts[2].name}</td>
+          <td>{contacts[2].popularity}</td>
+          <td>{contacts[2].wonOscar ? '🏆' : 'nope'}</td>
+          <td>{contacts[2].wonEmmy ? '🏆' : 'nope'}</td>
+        </tr>
+        <tr>
+        <td><img style={imgWidth} src={contacts[3].pictureUrl} alt=''></img></td>
+          <td>{contacts[3].name}</td>
+          <td>{contacts[3].popularity}</td>
+          <td>{contacts[3].wonOscar ? '🏆' : 'nope'}</td>
+          <td>{contacts[3].wonEmmy ? '🏆' : 'nope'}</td>
+        </tr>
+        <tr>
+        <td><img style={imgWidth} src={contacts[4].pictureUrl} alt=''></img></td>
+          <td>{contacts[4].name}</td>
+          <td>{contacts[4].popularity}</td>
+          <td>{contacts[4].wonOscar ? '🏆' : 'nope'}</td>
+          <td>{contacts[4].wonEmmy ? '🏆' : 'nope'}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 }
 
 export default App
